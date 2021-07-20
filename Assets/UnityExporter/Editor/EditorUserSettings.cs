71407cbb738759c14ec422089e8df97e152dc1d4
steft <52441940+stef-t@users.noreply.github.com>
2021-07-20 15:48:37 +0200

    moving classes to their separate files

---
+++ Assets/UnityExporter/Editor/EditorUserSettings.cs
@@ -0,0 +1,46 @@
+using UnityEditor;
+
+namespace UnityExporter
+{
+    /// <summary>
+    ///     Saves settings of the Editor user in <see cref="EditorUserBuildSettings" /> and <see cref="PlayerSettings" />
+    ///     before starting to build.
+    ///     This way these settings can be recovered after the build is complete.
+    /// </summary>
+    internal class EditorUserSettings
+    {
+        public EditorUserSettings()
+        {
+            buildTarget      = EditorUserBuildSettings.activeBuildTarget;
+            buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+            scriptingDefineSymbols =
+                PlayerSettings.GetScriptingDefineSymbolsForGroup(
+                    EditorUserBuildSettings.selectedBuildTargetGroup);
+            developmentBuildFlag      = EditorUserBuildSettings.development;
+            exportAsGoogleAndroidFlag = EditorUserBuildSettings.exportAsGoogleAndroidProject;
+        }
+
+        public BuildTarget      buildTarget               { get; private set; }
+        public BuildTargetGroup buildTargetGroup          { get; private set; }
+        public string           scriptingDefineSymbols    { get; private set; }
+        public bool             developmentBuildFlag      { get; private set; }
+        public bool             exportAsGoogleAndroidFlag { get; private set; }
+
+        public void Apply()
+        {
+            EditorUserBuildSettings.development                  = developmentBuildFlag;
+            EditorUserBuildSettings.exportAsGoogleAndroidProject = exportAsGoogleAndroidFlag;
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, scriptingDefineSymbols);
+        }
+
+        public override string ToString()
+        {
+            return
+                $"{nameof(buildTarget)}: {buildTarget}, "                       +
+                $"{nameof(buildTargetGroup)}: {buildTargetGroup}, "             +
+                $"{nameof(scriptingDefineSymbols)}: {scriptingDefineSymbols}, " +
+                $"{nameof(developmentBuildFlag)}: {developmentBuildFlag}, "     +
+                $"{nameof(exportAsGoogleAndroidFlag)}: {exportAsGoogleAndroidFlag}";
+        }
+    }
+}
--- Assets/UnityExporter/Editor/PlayerSettingsVersioner.cs
+++ Assets/UnityExporter/Editor/PlayerSettingsVersioner.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 namespace UnityExporter
 {
+    /// <summary>
+    ///     Helps managing version and version-code/build-number in <see cref="PlayerSettings" />.
+    /// </summary>
     internal class PlayerSettingsVersioner
     {
         public enum Bump
@@ -88,48 +91,6 @@ namespace UnityExporter
                 PlayerSettings.Android.bundleVersionCode >= 0                 &&
                 int.TryParse(PlayerSettings.iOS.buildNumber, out int iosCode) &&
                 iosCode >= 0;
-        }
-    }
-
-    /// <summary>
-    ///     Saves settings of the Editor user in <see cref="EditorUserBuildSettings" /> and <see cref="PlayerSettings" />
-    ///     before starting to build.
-    ///     This way these settings can be recovered after the build is complete.
-    /// </summary>
-    internal class EditorUserSettings
-    {
-        public EditorUserSettings()
-        {
-            buildTarget      = EditorUserBuildSettings.activeBuildTarget;
-            buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
-            scriptingDefineSymbols =
-                PlayerSettings.GetScriptingDefineSymbolsForGroup(
-                    EditorUserBuildSettings.selectedBuildTargetGroup);
-            developmentBuildFlag      = EditorUserBuildSettings.development;
-            exportAsGoogleAndroidFlag = EditorUserBuildSettings.exportAsGoogleAndroidProject;
-        }
-
-        public BuildTarget      buildTarget               { get; private set; }
-        public BuildTargetGroup buildTargetGroup          { get; private set; }
-        public string           scriptingDefineSymbols    { get; private set; }
-        public bool             developmentBuildFlag      { get; private set; }
-        public bool             exportAsGoogleAndroidFlag { get; private set; }
-
-        public void Apply()
-        {
-            EditorUserBuildSettings.development                  = developmentBuildFlag;
-            EditorUserBuildSettings.exportAsGoogleAndroidProject = exportAsGoogleAndroidFlag;
-            PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, scriptingDefineSymbols);
-        }
-
-        public override string ToString()
-        {
-            return
-                $"{nameof(buildTarget)}: {buildTarget}, "                       +
-                $"{nameof(buildTargetGroup)}: {buildTargetGroup}, "             +
-                $"{nameof(scriptingDefineSymbols)}: {scriptingDefineSymbols}, " +
-                $"{nameof(developmentBuildFlag)}: {developmentBuildFlag}, "     +
-                $"{nameof(exportAsGoogleAndroidFlag)}: {exportAsGoogleAndroidFlag}";
         }
     }
 }
