c551a1af233217aea8364734ac7b54b4d90463c9
steft <52441940+stef-t@users.noreply.github.com>
2021-07-20 13:41:36 +0200

    adding shell scripts that helps with managing unity package releases

---
+++ update-unity-package.sh
@@ -0,0 +1,14 @@
+git checkout master # making sure that we are on the master branch before starting merge
+git merge develop --no-edit
+
+# getting the version number out of the "package.json"
+# 1) finding the "package.json" file under "Assets"
+# 2) printing the file's contents
+# 3) getting the line containing the package's version
+# 4) getting the actual version number (note that "sed" requires extended regex for "+" to be recognised, therefore we simply use "[0-9][0-9]*" to let it know we want minium one digit)
+version=$(find Assets -name "package.json" | xargs cat | egrep "\"version\": " | sed 's/.*version.*\([0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\).*/\1/')
+
+git subtree split --prefix Assets/UnityExporter --branch upm
+git tag ${version} upm
+git push --all
+git push --tags
